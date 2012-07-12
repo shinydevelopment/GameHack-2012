512618f32991559e87072c5232e8f1d4d34009b8
Greg Spiers <greg.spiers@shinydevelopment.com>
2012-07-12 18:22:30 +0100

    Initial import

---
+++ GameHack/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  GameHack
+//
+//  Created by Greg Spiers on 12/07/2012.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
