06df3b287472968e0e5099f0ae5971e29cbbf460
Greg Spiers <greg.spiers@shinydevelopment.com>
2012-07-12 21:11:13 +0100

    Add touch support to animals.

--- GameHack/Sheep.m
+++ GameHack/Sheep.m
@@ -10,6 +10,7 @@ NSUInteger const SheepPoints = 100;
   self = [super init];
   if (self) {
     // Load sheep sprite
+    self.sprite = [CCSprite spriteWithFile:@"Icon@2x.png"];
   }
   return self;
 }
@@ -19,5 +20,25 @@ NSUInteger const SheepPoints = 100;
 {
   return SheepPoints;
 }
+
+#pragma mark Cocos2d drawing stuff
+
+//! called once per frame. time a value between 0 and 1
+//! For example:
+//! * 0 means that the action just started
+//! * 0.5 means that the action is in the middle
+//! * 1 means that the action is over
+-(void) update: (ccTime) time
+{
+  
+}
+
+#pragma mark Touch methods
+- (void) wasTouched
+{
+  // nothing in animal classes, subclasses implement
+  NSLog(@"Baaaa, I was touched");
+}
+
 
 @end
